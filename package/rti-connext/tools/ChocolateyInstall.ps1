f22dbf75b19b7de53c953f50a86d4061e5ad9fea
Thomas Moulard <tmoulard@amazon.com>
2019-01-12 23:44:11 -0800

    Adds RTI Connext 5.3 package
    Based on ROS 2 Source install documentation:
    https://index.ros.org/doc/ros2/Installation/Windows-Development-Setup/#rti-connext-5-3
    
    To build and install this package:
    $ cd packages/rti-connext
    $ choco pack
    $ cinst -s . rti-connext -y

---
+++ package/rti-connext/tools/ChocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = 'rti-connext'
+  url            = 'https://s3.amazonaws.com/RTI/Bundles/5.3.1/Evaluation/rti_connext_dds_secure-5.3.1-eval-i86Win32VS2017.zip'
+  url64          = 'https://s3.amazonaws.com/RTI/Bundles/5.3.1/Evaluation/rti_connext_dds_secure-5.3.1-eval-x64Win64VS2017.zip'
+  unzipLocation  = "C:\Program Files\rti_connext_dds-5.3.1"
+  checksumType   = 'sha256'
+  checksum       = '5B78DFAB5A57F542CA94C0F467462430D850C0D94EC13FC06E60A118391220C0'
+  checksumType64 = 'sha256'
+  checksum64     = 'CAAED4527D2A00F60BCC637742B73FE7C7CAE9B57809A9BBF2BE1C8D729FDE79'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+& $(Join-Path $packageArgs.unzipLocation resource\scripts\rtisetenv_x64Win64VS2017.bat)
+Install-ChocolateyEnvironmentVariable NDDSHOME "$packageArgs.unzipLocation" Machine
