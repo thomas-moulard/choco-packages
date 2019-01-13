824d63380339078c84cfc087fdaa820158d7029b
Thomas Moulard <tmoulard@amazon.com>
2019-01-13 00:37:09 -0800

    Adds Qt 5.10.0 package
    Based on ROS 2 Source install documentation:
    https://index.ros.org/doc/ros2/Installation/Windows-Development-Setup/#install-qt5
    
    This package is a Chocolatey automatic package:
    https://chocolatey.org/docs/automatic-packages
    
    To build and install this package:
    
    $ cinst au
    $ cd packages/qt-sdk
    $ ./update.ps1
    $ Get-RemoteFiles -Purge -NoSuffix
    $ choco pack
    $ cinst -s . qt-sdk -y
    
    This package only installs the Qt 5.10.0 msvc2017 64-bits component to save disk space.
    Installing all components fail on my machine.

---
+++ package/qt-sdk/tools/ChocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = 'Qt SDK'
+  fileType       = 'exe'
+  file           = "$toolsPath\qt-opensource-windows-x86-5.10.0.exe"
+  softwareName   = 'Qt*'
+  silentArgs     = "--script $toolsPath\installer-noninteractive.qs"
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item -Force -ea 0 "$toolsPath\*.exe","$toolsPath\*.ignore"
+
+$installLocation = Get-AppInstallLocation $packageArgs.softwareName
+Write-Host "Installed to '$installLocation'"
+Install-ChocolateyEnvironmentVariable Qt5_DIR "$(Join-Path $installLocation msvc2017_64)" Machine
