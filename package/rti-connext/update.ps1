194564a645234582b3a6991c39295cf532191198
Thomas Moulard <tmoulard@amazon.com>
2019-01-12 23:44:11 -0800

    Adds RTI Connext 5.3 package
    Based on ROS 2 Source install documentation:
    https://index.ros.org/doc/ros2/Installation/Windows-Development-Setup/#rti-connext-5-3
    
    This package is a Chocolatey automatic package:
    https://chocolatey.org/docs/automatic-packages
    
    To build and install this package:
    $ cinst au
    $ cd packages/rti-connext
    $ ./update.ps1
    $ Get-RemoteFiles -Purge -NoSuffix
    $ choco pack
    $ cinst -s . rti-connext -y

---
+++ package/rti-connext/update.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module AU
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+function global:au_GetLatest {
+  @{
+    URL32 = "https://s3.amazonaws.com/RTI/Bundles/5.3.1/Evaluation/rti_connext_dds_secure-5.3.1-eval-i86Win32VS2017.zip"
+    URL64 = "https://s3.amazonaws.com/RTI/Bundles/5.3.1/Evaluation/rti_connext_dds_secure-5.3.1-eval-x64Win64VS2017.zip"
+    Version = "5.3.1"
+    PackageName = 'RTI Connext'
+  }
+}
+
+update -ChecksumFor none
